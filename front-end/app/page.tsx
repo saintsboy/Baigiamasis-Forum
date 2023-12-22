3cd480a83a294c4c8941a26a5fad641649946a06
Lukas <sutkuslukas@gmail.com>
2023-12-22 07:51:33 +0200

    added examples

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -19,3 +19,70 @@ export default function Home() {
   }, []);
   return <div>{questions && questions[0].question_text}</div>;
 }
+
+// import React, { useState } from "react";
+// import axios from "axios";
+// import cookie from "js-cookie";
+// import { useRouter } from "next/router";
+
+// import styles from "./styles.module.css";
+// import PageTemplate from "@/components/PageTemplate/PageTemplate";
+
+// const Register = () => {
+//   const router = useRouter();
+
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const onRegister = async () => {
+//     const body = {
+//       name: name,
+//       email: email,
+//       password: password,
+//     };
+
+//     try {
+//       const response = await axios.post(
+//         `${process.env.SERVER_URL}/users/register`,
+//         body
+//       );
+
+//       if (response.status === 200) {
+//         cookie.set("jwt_token", response.data.token);
+//         router.push("/");
+//       }
+
+//       console.log("response", response);
+//     } catch (error) {
+//       console.error("Error registering:", error);
+//       // Handle registration error, e.g., display an error message
+//     }
+//   };
+
+//   return (
+//     <PageTemplate>
+//       <div className={styles.form}>
+//         <input
+//           placeholder="name"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//         />
+//         <input
+//           placeholder="email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//         />
+//         <input
+//           placeholder="password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//           type="password"
+//         />
+//         <button onClick={onRegister}>Register</button>
+//       </div>
+//     </PageTemplate>
+//   );
+// };
+
+// export default Register;
